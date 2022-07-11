38ef6f17c32b98ad59a2ddbde2a9915071998967
SEC <gunwo151@gmail.com>
2022-07-11 15:39:45 +0900

    Initial commit

---
+++ src/main/java/jenkins/JenkinsDeployProject/JenkinsDeployProjectApplication.java
@@ -0,0 +1,13 @@
+package jenkins.JenkinsDeployProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeployProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDeployProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jenkins/JenkinsDeployProject/JenkinsDeployProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins.JenkinsDeployProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeployProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
